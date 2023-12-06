66cdca61b230b59e1126d7ce084515c55cf84f7c
Alex <soportealexsr@gmail.com>
2023-12-06 12:54:39 -0600

    first commit

---
+++ sensor1/sensor1.ino
@@ -0,0 +1,21 @@
+int SENSOR = 2;
+int LED = 3;
+int ESTADO;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(SENSOR, INPUT);
+pinMode(SENSOR, OUTPUT);
+digitalWrite(LED,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ESTADO = digitalRead(SENSOR);
+  if (ESTADO ==LOW){
+    digitalWrite(LED, HIGH);
+    delay(500);
+    }
+  digitalWrite(LED, LOW);
+}
